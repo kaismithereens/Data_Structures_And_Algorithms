19753078a00294e25e8d2a852fe3886d4bda691a
Kai <kai.nevermind@gmail.com>
2023-12-29 13:34:14 +0100

    Created stack class with pop, push, peak, size and is_empty methods.

--- Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/stack.py
+++ Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/stack.py
@@ -6,4 +6,36 @@ Robin Andrews - https://compucademy.net/
 
 
 class Stack:
-    pass
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+        #return not self.items
+    
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peak(self):
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+    
+    def __str__(self):
+        return str(self.items)
+
+
+if __name__ == "__main__":
+    s = Stack()
+    print(s)
+
+    my_stack = Stack()
+    my_stack.push(1)
+    my_stack.push(2)
+    my_stack.push(2)
+    my_stack.pop()
+    print(my_stack)
