a949fba753e0b49edf44924a66ae9eccbd63f885
Kai <kai.nevermind@gmail.com>
2023-12-31 08:59:00 +0100

    Added code to reverse string using Stack methods pop and push

--- Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/reverse_string.py
+++ Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/reverse_string.py
@@ -9,7 +9,15 @@ import stack
 string = "gninraeL nIdekniL htiw tol a nraeL"
 reversed_string = ""
 s = stack.Stack()
+my_new_stack = stack.Stack()
 
-# Your solution here.
+s.push(string)
+print(s)
+
+for char in string:
+    my_new_stack.push(char)
+
+while not my_new_stack.is_empty():
+    reversed_string += my_new_stack.pop()
 
 print(reversed_string)
